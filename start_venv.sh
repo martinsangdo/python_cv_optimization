fd3c6cabd6b44838132782efa8a95bce579dea05
martinsangdo <dothanhsang1984@yahoo.com>
2026-03-20 15:47:22 +0700

    - create virtual env for the project

---
+++ start_venv.sh
@@ -0,0 +1,4 @@
+#create new virtual environment: /opt/homebrew/bin/python3.13 -m venv myenv313
+#install kernel in Jypyter: python -m ipykernel install --user --name=myenv313
+#. ./start_venv.sh
+source myenv313/bin/activate
